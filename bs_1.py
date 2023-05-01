275f64dc9fbf8e023bba7af558706cb4c7334e2a
udenis80 <udenis80@mail.ru>
2023-05-02 01:04:08 +0300

    set parser for films

--- bs_1.py
+++ bs_1.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
-
+data = []
 url = "https://www.kinopoisk.ru/lists/movies/top250/"
 r = requests.get(url)
-soup=BeautifulSoup(r.text, 'lxml')
-print(soup)
+soup = BeautifulSoup(r.text, 'lxml')
+# for film in range:
+print(soup.find('div', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj'))
+print(soup.find('a', class_='base-movie-main-info_link__YwtP1'))
